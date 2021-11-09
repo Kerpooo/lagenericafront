ba2306a0044ec27e4593cad9ba6dbd1ee030f89d
Kevin Rincon <kevinrincon8@gmail.com>
2021-11-08 23:54:56 -0500

    Se corregieron imports del app y productos

--- router/Productos.js
+++ router/Productos.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+//Cosas de productos
+const Papa = require("papaparse");
+const multer = require("multer");
+const fs = require("fs");
+const http = require("http");
+
+//Para subir archivos
+const upload = multer({ dest: "./" });
+
+
 router.get('/', (req,res) => {
     res.render("productos")
 })
