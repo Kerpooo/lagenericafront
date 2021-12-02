d49ba2753d045d1e0cf1cb63e7a9df9e137b306d
kevin rincon <kevinrincon8@gmail.com>
2021-12-02 14:48:01 -0500

    Arreglar los metodos

--- router/Clientes.js
+++ router/Clientes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
+const http = require("http");
 const app = express();
 
 // create application/json parser
@@ -8,6 +9,28 @@ const jsonParser = bodyParser.json();
 
 // create application/x-www-form-urlencoded parser
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+//Cosas de clientes
+
+router.get("/", (req, res) => {
+	res.render("templates/vistaClientes/clientes");
+});
+
+// Genera la vista de consultar
+
+router.get("/consultar", (req, res) => {
+	res.render("templates/vistaClientes/consultar");
+});
+
+// Genera la vista de crear
+router.get("/crear", (req, res) => {
+	res.render("templates/vistaClientes/crear");
+});
+
+// Genera la vista de actualizar
+router.get("/actualizar", (req, res) => {
+	res.render("templates/vistaClientes/actualizar");
+});
 
 //RequestBack
 function backServerReq(options, data) {
@@ -34,38 +57,12 @@ function backServerReq(options, data) {
 	req.end();
 }
 
-//Cosas de clientes
-const http = require("http");
-
-router.get("/", (req, res) => {
-	res.render("templates/vistaClientes/clientes");
-});
-
-// Genera la vista de consultar
-
-router.get("/consultar", (req, res) => {
-    res.render("templates/vistaClientes/consultar");
-});
-
-// Genera la vista de crear
-router.get("/crear", (req, res) => {
-    res.render("templates/vistaClientes/crear");
-});
-
-// Genera la vista de actualizar
-router.get("/actualizar", (req, res) => {
-    res.render("templates/vistaClientes/actualizar");
-});
-
-router.post("/", urlencodedParser, async (req, res) => {
+router.post("/crear", urlencodedParser, async (req, res) => {
 	const nombreCompleto = req.body.nombreCompleto;
 	const cedula = req.body.cedula;
 	const correo = req.body.correo;
 	const telefono = req.body.telefono;
 	const direccion = req.body.direccion;
-	const eliminar = req.body.eliminar;
-	const buscar = req.body.buscar;
-	const crear = req.body.crear;
 
 	const data = JSON.stringify({
 		cedulaCliente: cedula,
@@ -77,14 +74,44 @@ router.post("/", urlencodedParser, async (req, res) => {
 
 	//DEBUGGER
 	console.log(req.body);
-	console.log(eliminar == "");
+
+	//Para Crear Cliente
+
+	const options = {
+		host: "localhost",
+		port: 8082,
+		path: "/api/crear",
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Content-Length": data.length,
+		},
+	};
+
+	//Alerta Cliente Creado Correcto
+	res.render("./templates/vistaClientes/crear", {
+		alerta: "Cliente Creado",
+		colorAlerta: "success",
+	});
+});
+
+router.post("/", urlencodedParser, async (req, res) => {
+	const cedula = req.body.cedula;
+	const eliminar = req.body.eliminar;
+	const buscar = req.body.buscar;
+
+	const data = JSON.stringify({
+		cedulaCliente: cedula,
+	});
 
 	//Para consulta por cedula
 	if (buscar == "") {
+		console.log(req.body);
+
 		const options = {
 			host: "localhost",
 			port: 8082,
-			path: "/api/consultar/"+cedula,
+			path: "/api/consultar/" + cedula,
 			method: "GET",
 			headers: {
 				"Content-Type": "application/json",
@@ -92,13 +119,11 @@ router.post("/", urlencodedParser, async (req, res) => {
 			},
 		};
 
-		backServerReq(options, data);
+		let resultado = backServerReq(options, data);
 
-		//Alerta Consulta
-		res.render("./templates/vistaClientes/clientes", {
-			alerta: "Consulta",
-			colorAlerta: "warning",
-		});
+		console.log("DEbug" + resultado);
+
+		res.render("templates/vistaClientes/consultar", {});
 	}
 
 	//Eliminar
@@ -107,7 +132,7 @@ router.post("/", urlencodedParser, async (req, res) => {
 		const options = {
 			host: "localhost",
 			port: 8082,
-			path: "/api/eliminar/"+cedula,
+			path: "/api/eliminar/" + cedula,
 			method: "DELETE",
 			headers: {
 				"Content-Type": "application/json",
@@ -123,28 +148,5 @@ router.post("/", urlencodedParser, async (req, res) => {
 			colorAlerta: "danger",
 		});
 	}
-
-	//Para Crear Cliente
-	if (crear == "") {
-		const options = {
-			host: "localhost",
-			port: 8082,
-			path: "/api/crear",
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				"Content-Length": data.length,
-			},
-		};
-
-		backServerReq(options, data);
-
-		//Alerta Cliente Creado Correcto
-		res.render("./templates/vistaClientes/clientes", {
-			alerta: "Cliente Creado",
-			colorAlerta: "success",
-		});
-	}
 });
-
 module.exports = router;
