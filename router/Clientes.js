be72b6bf76a4328e6f6a92be7948d6750f47cd93
Johnsaurios <alcr.john.galindo@gmail.com>
2021-12-01 22:01:42 -0500

    Modificacion de diseños cliente
    se realiza modificación de estructura de diseño

--- router/Clientes.js
+++ router/Clientes.js
@@ -5,8 +5,30 @@ const router = express.Router();
 const fs = require("fs");
 const http = require("http");
 
+// Genera la vista del menu de clientes
 router.get("/", (req, res) => {
     res.render("templates/vistaClientes/clientes");
+});
+
+// Genera la vista de consultar
+
+router.get("/consultar", (req, res) => {
+    res.render("templates/vistaClientes/consultar");
+});
+
+// Genera la vista de crear
+router.get("/crear", (req, res) => {
+    res.render("templates/vistaClientes/crear");
+});
+
+// Genera la vista de actualizar
+router.get("/actualizar", (req, res) => {
+    res.render("templates/vistaClientes/actualizar");
+});
+
+// Genera la vista de eliminar
+router.get("/eliminar", (req, res) => {
+    res.render("templates/vistaClientes/eliminar");
 });
 
 module.exports = router;
